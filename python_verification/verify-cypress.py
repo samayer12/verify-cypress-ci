3d06ba21f242b4db0a85b5d5d553db61f6bc8b8f
Sam Mayer <samayer12@gmail.com>
2024-02-29 17:27:26 -0600

    Update .gitignore

--- python_verification/verify-cypress.py
+++ python_verification/verify-cypress.py
@@ -6,10 +6,11 @@ writes to an output file and parses command line arguments
 from __future__ import print_function
 import argparse
 import json
-from datetime import date, datetime, timezone
+import sys
+from datetime import datetime
 
 
-def main():
+def parse_args(args):
     parser = argparse.ArgumentParser(description=__doc__)
 
     parser.add_argument(
@@ -21,21 +22,25 @@ def main():
         "output", nargs="?", default="-",
         metavar="OUTPUT_FILE", type=argparse.FileType("w"),
         help="path to the output file (write to stdout if omitted)")
+    return parser.parse_args(args)
 
-    args = parser.parse_args()
+
+def main():
+    args = parse_args(sys.argv[1:])
 
     report_file = args.input.name
-    output_file = args.output.name
 
     report_data = json.load(open(report_file, 'r'))
     pass_percent = report_data["stats"]["passPercent"]
     start_time = datetime.fromisoformat(report_data["stats"]["start"])
 
-    print("Pass Percent: {} @ {}".format(pass_percent, start_time))
     if pass_percent != 100:
         raise ValueError("Pass Percent is not 100%")
     if start_time.date() < datetime.utcnow().date():
         raise ValueError("Looks like tests weren't ran today")
+    else:
+        print("Cypress Test Status: {}% @ {} from input '{}'".format(pass_percent, start_time, report_file))
+
 
 if __name__ == "__main__":
     main()
